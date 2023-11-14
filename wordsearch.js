615a9ba8c36a205bf9fb57474731b3f5684ff27b
EdmoreMafuwe <126406684+EdmoreMafuwe@users.noreply.github.com>
2023-11-14 02:14:22 -0800

    Implement wordSearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    // Check horizontally
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
     }
-}
 
-module.exports = wordSearch
+    // Check vertically
+    for (let i = 0; i < letters[0].length; i++) {
+        const verticalJoin = letters.map(ls => ls[i]).join('');
+        if (verticalJoin.includes(word)) return true;
+    }
+
+    // If the word is not found horizontally or vertically, return false
+    return false;
+};
+
+module.exports = wordSearch;
